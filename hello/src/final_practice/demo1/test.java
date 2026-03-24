9d80f287f6f6078169e9f5b4a9f018ce78c38650
陈宇 <3128607571@qq.com>
2026-03-24 17:11:39 +0800

    practice

--- hello/src/final_practice/demo1/test.java
+++ hello/src/final_practice/demo1/test.java
@@ -1,7 +1,32 @@
 package final_practice.demo1;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class test {
     public static void main(String[] args) {
+        TreeSet<Integer> red = new TreeSet<>();
+        while(red.size() < 6){
+            red.add((int)(Math.random() * 35 + 1));
+        }
+        System.out.println(red);
 
+        int blue = (int)(Math.random() * 15 + 1);
+        System.out.println(blue);
+
+        Set<Integer> luckeyred = new TreeSet<>();
+        Collections.addAll(luckeyred, 10, 12, 30, 16, 7, 17);
+
+        int coount = 0;
+        for (Integer num : red) {
+            if (luckeyred.contains(num)) {
+                coount++;
+            }
+        }
+        System.out.println("中奖个数：" + coount);
+
+        int luckeyblue = 12;
+        System.out.println("中奖蓝球：" + (luckeyblue == blue ? "1" : "0"));
     }
 }
--- hello/src/final_practice/demo2/test.java
+++ hello/src/final_practice/demo2/test.java
@@ -1,7 +1,53 @@
 package final_practice.demo2;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class test {
     public static void main(String[] args) {
+        List<prisoner> list = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) {
+            int code = (int) (Math.random() * 200) + 1;
+            if (isPrisoner(list, code)) {
+                i--;
+                continue;
+            }
+            prisoner p = new prisoner(code, i);
+            list.add(p);
+        }
+        System.out.println(list);
 
+        while(true){
+            List<prisoner> list1 = new ArrayList<>();
+            for (int i = 1; i < list.size(); i+=2) {
+                list1.add(list.get(i));
+            }
+            if(list1.size() == 1){
+                System.out.println(list1.get(0));
+            }
+            list = list1;
+        }
     }
+
+    public static boolean isPrisoner(List<prisoner> list, int code) {
+        for (prisoner p : list) {
+            if (p.getCode() == code) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class prisoner {
+    private int code;
+    private int id;
 }
